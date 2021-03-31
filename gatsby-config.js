e8e9e2c35562bf07d048c26d498a85c5f7b1f121
Fiona Gridley <gridley.f@northeastern.edu>
2021-03-31 00:05:34 -0500

    initialize gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  flags: {
+    DEV_SSR: false,
+  },
+  siteMetadata: {
+    title: "Fiona Gridley's Portfolio",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-mdx",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
