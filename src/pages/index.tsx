fafbce71039d214ab1022ef2738f4a3e41764eca
Fiona Gridley <gridley.f@northeastern.edu>
2021-04-03 17:38:21 -0500

    add basic pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import Layout from '../components/layout';
+
+const IndexPage = (): JSX.Element => {
+  return (
+    <Layout>
+      <h1>Hello World</h1>
+    </Layout>
+  );
+};
+
+export default IndexPage;
